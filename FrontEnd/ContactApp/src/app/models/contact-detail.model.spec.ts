1b8a4a7a02f7fd73eb305978905b5df67b683d53
dubeyyogesh10 <dubeyyogesh10@gmail.com>
2021-07-19 05:04:09 +0530

    front build

--- FrontEnd/ContactApp/src/app/models/contact-detail.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ContactDetail } from './contact-detail.model';
-
-describe('ContactDetail', () => {
-  it('should create an instance', () => {
-    expect(new ContactDetail()).toBeTruthy();
-  });
-});
